e0f5432afd9636a68d8368ff56e25e00c0dcbb37
dscitb <admin@agroprecision.com>
2015-03-22 17:11:47 -0300

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+# Rprogramming
+Repository for course assignments
+=======
 ### Introduction
 
 This second programming assignment will require you to write an R
@@ -103,3 +107,4 @@ In order to complete this assignment, you must do the following:
 ### Grading
 
 This assignment will be graded via peer assessment.
+>>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,35 @@
 
 ## Write a short comment describing this function
 
+
+#
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    
+    # change matrix
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    # to check if the matriz has been cached
+    get <- function() {
+        x
+    } 
+    # save inverse matrix calculation to cache
+    setinverse <- function(z){
+        inverse <<- z
+    }
+    # returns cached inverse matrix
+    getinverse <- function (){
+        inverse
+    }
+    # returns special matrix
+    list(set=set,
+         get=get,   
+         setinverse=setinverse, 
+         getinverse=getinverse
+    )
+
 
 }
 
@@ -12,4 +40,32 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv<-x$getinverse()
+        if (!is.null(inv)){
+            # inverse is cached
+            message("Inverse is cached")
+            return(inv) 
+        }
+        # else
+        mat<-x$get()
+        inv<-solve(mat)   #solve inverse calc
+        x$setinverse(inv) # cache inverse calc
+        return(i)
 }
+ 
+
+#> x = rbind(c(1, 0,1), c(1,0, -1),c(-1,1,0))
+#> m$set(x)
+#> cacheSolve(m)
+#     [,1] [,2] [,3]
+#[1,]  0.5  0.5    0
+#[2,]  0.5  0.5    1
+#[3,]  0.5 -0.5    0
+#> cacheSolve(m)
+#Inverse is cached
+#     [,1] [,2] [,3]
+#[1,]  0.5  0.5    0
+#[2,]  0.5  0.5    1
+#[3,]  0.5 -0.5    0
+        
+
